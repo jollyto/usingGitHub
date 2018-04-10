d5873c028237a0387fdf61e0627248031f9af969
Jose Rivera <jollyto@me.com>
2018-04-10 11:02:32 -0400

    Initial Commit

---
+++ usingGitHub/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  usingGitHub
+//
+//  Created by Jose Rivera on 4/10/18.
+//  Copyright © 2018 Jose Rivera. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
